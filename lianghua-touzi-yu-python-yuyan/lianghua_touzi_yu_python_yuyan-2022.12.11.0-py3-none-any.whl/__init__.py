82ddd10b827e106accf36508ffb4943cd92981a0
Tom Forbes <tom@tomforb.es>
2022-12-11 07:09:50 +0000

    {"name":"lianghua-touzi-yu-python-yuyan","version":"2022.12.11.0","file":"lianghua_touzi_yu_python_yuyan-2022.12.11.0-py3-none-any.whl","path":"lianghua-touzi-yu-python-yuyan/lianghua_touzi_yu_python_yuyan-2022.12.11.0-py3-none-any.whl"}

---
+++ lianghua-touzi-yu-python-yuyan/lianghua_touzi_yu_python_yuyan-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lianghua-touzi-yu-python-yuyan
+https://github.com/apachecn/lianghua-touzi-yu-python-yuyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
