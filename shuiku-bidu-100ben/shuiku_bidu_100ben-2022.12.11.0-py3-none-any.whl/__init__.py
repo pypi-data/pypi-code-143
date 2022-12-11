990ddb62e2d2ae5fda9bc0f1cfb76ccfb6d63bfb
Tom Forbes <tom@tomforb.es>
2022-12-11 07:13:59 +0000

    {"name":"shuiku-bidu-100ben","version":"2022.12.11.0","file":"shuiku_bidu_100ben-2022.12.11.0-py3-none-any.whl","path":"shuiku-bidu-100ben/shuiku_bidu_100ben-2022.12.11.0-py3-none-any.whl"}

---
+++ shuiku-bidu-100ben/shuiku_bidu_100ben-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiku-bidu-100ben
+https://github.com/apachecn/shuiku-bidu-100ben"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
