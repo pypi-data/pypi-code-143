ae09e8f4a93d0905865593580ce55d8974656255
Tom Forbes <tom@tomforb.es>
2022-12-11 07:10:24 +0000

    {"name":"python-jiqi-xuexi-yulianghua-touzi","version":"2022.12.11.0","file":"python_jiqi_xuexi_yulianghua_touzi-2022.12.11.0-py3-none-any.whl","path":"python-jiqi-xuexi-yulianghua-touzi/python_jiqi_xuexi_yulianghua_touzi-2022.12.11.0-py3-none-any.whl"}

---
+++ python-jiqi-xuexi-yulianghua-touzi/python_jiqi_xuexi_yulianghua_touzi-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-jiqi-xuexi-yulianghua-touzi
+https://github.com/apachecn/python-jiqi-xuexi-yulianghua-touzi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
