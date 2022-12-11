e32c35b497209f491478dffa69ca36f26f1f43ed
Tom Forbes <tom@tomforb.es>
2022-12-11 06:54:23 +0000

    {"name":"beizhebide-zhenxiang","version":"2022.12.11.0","file":"beizhebide_zhenxiang-2022.12.11.0-py3-none-any.whl","path":"beizhebide-zhenxiang/beizhebide_zhenxiang-2022.12.11.0-py3-none-any.whl"}

---
+++ beizhebide-zhenxiang/beizhebide_zhenxiang-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""beizhebide-zhenxiang
+https://github.com/apachecn/beizhebide-zhenxiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
