30af4fc5ba6bc0295ae36a9a27ba841d3eb8363e
Tom Forbes <tom@tomforb.es>
2022-12-11 07:21:16 +0000

    {"name":"zhongguo-shehui-gejieceng-xinli-fenxi","version":"2022.12.11.0","file":"zhongguo_shehui_gejieceng_xinli_fenxi-2022.12.11.0-py3-none-any.whl","path":"zhongguo-shehui-gejieceng-xinli-fenxi/zhongguo_shehui_gejieceng_xinli_fenxi-2022.12.11.0-py3-none-any.whl"}

---
+++ zhongguo-shehui-gejieceng-xinli-fenxi/zhongguo_shehui_gejieceng_xinli_fenxi-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhongguo-shehui-gejieceng-xinli-fenxi
+https://github.com/apachecn/zhongguo-shehui-gejieceng-xinli-fenxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
