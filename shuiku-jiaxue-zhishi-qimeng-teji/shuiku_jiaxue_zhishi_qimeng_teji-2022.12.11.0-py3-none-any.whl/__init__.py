b09a25f9b1f5f4e23326766d94c6cb4ecc2942e7
Tom Forbes <tom@tomforb.es>
2022-12-11 07:17:08 +0000

    {"name":"shuiku-jiaxue-zhishi-qimeng-teji","version":"2022.12.11.0","file":"shuiku_jiaxue_zhishi_qimeng_teji-2022.12.11.0-py3-none-any.whl","path":"shuiku-jiaxue-zhishi-qimeng-teji/shuiku_jiaxue_zhishi_qimeng_teji-2022.12.11.0-py3-none-any.whl"}

---
+++ shuiku-jiaxue-zhishi-qimeng-teji/shuiku_jiaxue_zhishi_qimeng_teji-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiku-jiaxue-zhishi-qimeng-teji
+https://github.com/apachecn/shuiku-jiaxue-zhishi-qimeng-teji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
