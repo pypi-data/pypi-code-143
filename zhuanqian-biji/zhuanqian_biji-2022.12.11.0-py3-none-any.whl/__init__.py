8fe6627ca7b1d87c07bc622edfb2d777d8e83977
Tom Forbes <tom@tomforb.es>
2022-12-11 07:21:26 +0000

    {"name":"zhuanqian-biji","version":"2022.12.11.0","file":"zhuanqian_biji-2022.12.11.0-py3-none-any.whl","path":"zhuanqian-biji/zhuanqian_biji-2022.12.11.0-py3-none-any.whl"}

---
+++ zhuanqian-biji/zhuanqian_biji-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhuanqian-biji
+https://github.com/apachecn/zhuanqian-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
