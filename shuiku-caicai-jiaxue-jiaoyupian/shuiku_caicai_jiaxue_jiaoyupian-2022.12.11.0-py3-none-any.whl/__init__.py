2194cbaebba780434bad1a2e1f279605275b38aa
Tom Forbes <tom@tomforb.es>
2022-12-11 07:14:11 +0000

    {"name":"shuiku-caicai-jiaxue-jiaoyupian","version":"2022.12.11.0","file":"shuiku_caicai_jiaxue_jiaoyupian-2022.12.11.0-py3-none-any.whl","path":"shuiku-caicai-jiaxue-jiaoyupian/shuiku_caicai_jiaxue_jiaoyupian-2022.12.11.0-py3-none-any.whl"}

---
+++ shuiku-caicai-jiaxue-jiaoyupian/shuiku_caicai_jiaxue_jiaoyupian-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiku-caicai-jiaxue-jiaoyupian
+https://github.com/apachecn/shuiku-caicai-jiaxue-jiaoyupian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
