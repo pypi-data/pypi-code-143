48289f067bc08230892480b75571c2352f4b246d
Tom Forbes <tom@tomforb.es>
2022-12-11 07:16:38 +0000

    {"name":"shuiku-jiaxue-wenji-mouluepian","version":"2022.12.11.0","file":"shuiku-jiaxue-wenji-mouluepian-2022.12.11.0.tar.gz","path":"shuiku-jiaxue-wenji-mouluepian/shuiku-jiaxue-wenji-mouluepian-2022.12.11.0.tar.gz"}

---
+++ shuiku-jiaxue-wenji-mouluepian/shuiku-jiaxue-wenji-mouluepian-2022.12.11.0.tar.gz/ShuikuJiaxueWenjiMouluepian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiku-jiaxue-wenji-mouluepian
+https://github.com/apachecn/shuiku-jiaxue-wenji-mouluepian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
