c28465a2eac5163d9fc1ea4c1429cecca43ea368
Tom Forbes <tom@tomforb.es>
2022-12-11 07:12:05 +0000

    {"name":"sanhu-touzi-shangshou-diyibenshu","version":"2022.12.11.0","file":"sanhu_touzi_shangshou_diyibenshu-2022.12.11.0-py3-none-any.whl","path":"sanhu-touzi-shangshou-diyibenshu/sanhu_touzi_shangshou_diyibenshu-2022.12.11.0-py3-none-any.whl"}

---
+++ sanhu-touzi-shangshou-diyibenshu/sanhu_touzi_shangshou_diyibenshu-2022.12.11.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sanhu-touzi-shangshou-diyibenshu
+https://github.com/apachecn/sanhu-touzi-shangshou-diyibenshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.11.0"
