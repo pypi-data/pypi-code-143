1b38763dafebdcb1ba080ae4309128acad531979
Tom Forbes <tom@tomforb.es>
2022-12-11 07:13:53 +0000

    {"name":"shiyong-geshang","version":"2022.12.11.0","file":"shiyong-geshang-2022.12.11.0.tar.gz","path":"shiyong-geshang/shiyong-geshang-2022.12.11.0.tar.gz"}

---
+++ shiyong-geshang/shiyong-geshang-2022.12.11.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ShiyongGeshang
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ShiyongGeshang'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="shiyong-geshang",
+    version=ShiyongGeshang.__version__,
+    url="https://github.com/apachecn/shiyong-geshang",
+    author=ShiyongGeshang.__author__,
+    author_email=ShiyongGeshang.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="石勇：格商",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "shiyong-geshang=ShiyongGeshang.__main__:main",
+            "ShiyongGeshang=ShiyongGeshang.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
