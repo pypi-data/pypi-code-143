847a9bfa7331923f12561a8f0468c1caf2db970c
Tom Forbes <tom@tomforb.es>
2022-12-12 12:30:17 +0000

    {"name":"moesif-blog-zh","version":"2022.12.12.0","file":"moesif-blog-zh-2022.12.12.0.tar.gz","path":"moesif-blog-zh/moesif-blog-zh-2022.12.12.0.tar.gz"}

---
+++ moesif-blog-zh/moesif-blog-zh-2022.12.12.0.tar.gz/MoesifBlogZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""moesif-blog-zh
+https://github.com/apachecn/moesif-blog-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.12.0"
