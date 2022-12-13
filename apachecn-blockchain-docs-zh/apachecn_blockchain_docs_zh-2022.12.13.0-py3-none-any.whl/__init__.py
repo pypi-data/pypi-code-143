7fd33b7adabc2f371d0793eebe85f12329215105
Tom Forbes <tom@tomforb.es>
2022-12-13 10:26:47 +0000

    {"name":"apachecn-blockchain-docs-zh","version":"2022.12.13.0","file":"apachecn_blockchain_docs_zh-2022.12.13.0-py3-none-any.whl","path":"apachecn-blockchain-docs-zh/apachecn_blockchain_docs_zh-2022.12.13.0-py3-none-any.whl"}

---
+++ apachecn-blockchain-docs-zh/apachecn_blockchain_docs_zh-2022.12.13.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-blockchain-docs-zh
+https://github.com/apachecn/apachecn-blockchain-docs-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.13.0"
